2dba98154b8149e8b87387bb1f6f7e8871076c4f
Jamie Wright <jamie@brilliantfantastic.com>
2021-10-06 15:24:52 -0400

    Add client options

--- lib/omniauth/strategies/webex.rb
+++ lib/omniauth/strategies/webex.rb
@@ -4,6 +4,9 @@ module OmniAuth
   module Strategies
     class Webex < OmniAuth::Strategies::OAuth2
       option :name, "webex"
+      option :client_options,
+        site: "https://webexapis.com",
+        authorize_path: "/v1/authorize"
     end
   end
 end
--- spec/omniauth/strategies/webex_spec.rb
+++ spec/omniauth/strategies/webex_spec.rb
@@ -2,12 +2,22 @@ require "spec_helper"
 
 RSpec.describe OmniAuth::Strategies::Webex do
   subject(:strategy) do
-    OmniAuth::Strategies::Webex.new("appid", "secret", @options || {})
+    OmniAuth::Strategies::Webex.new("appid", "secret")
   end
 
   describe "#options" do
     it "has the correct name" do
       expect(strategy.options.name).to eq "webex"
+    end
+
+    describe "#client_options" do
+      it "has the correct site" do
+        expect(strategy.options.client_options.site).to eq "https://webexapis.com"
+      end
+
+      it "has the correct authorize path" do
+        expect(strategy.options.client_options.authorize_path).to eq "/v1/authorize"
+      end
     end
   end
 end
