9589dd05742af2b0610bf8369978e4248ebc3b12
Jamie Wright <jamie@brilliantfantastic.com>
2021-10-07 11:32:34 -0400

    Update to the correct options

--- lib/omniauth/strategies/webex.rb
+++ lib/omniauth/strategies/webex.rb
@@ -4,9 +4,17 @@ module OmniAuth
   module Strategies
     class Webex < OmniAuth::Strategies::OAuth2
       option :name, "webex"
+
+      option :authorize_options, [:scope]
+
       option :client_options,
         site: "https://webexapis.com",
-        authorize_path: "/v1/authorize"
+        authorize_url: "https://webexapis.com/v1/authorize",
+        token_url: "https://webexapis.com/v1/access_token"
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
--- spec/omniauth/strategies/webex_spec.rb
+++ spec/omniauth/strategies/webex_spec.rb
@@ -10,13 +10,23 @@ RSpec.describe OmniAuth::Strategies::Webex do
       expect(strategy.options.name).to eq "webex"
     end
 
+    describe "#authorize_options" do
+      it "includes a scope option" do
+        expect(strategy.options.authorize_options).to include :scope
+      end
+    end
+
     describe "#client_options" do
       it "has the correct site" do
         expect(strategy.options.client_options.site).to eq "https://webexapis.com"
       end
 
-      it "has the correct authorize path" do
-        expect(strategy.options.client_options.authorize_path).to eq "/v1/authorize"
+      it "has the correct authorize url" do
+        expect(strategy.options.client_options.authorize_url).to eq "https://webexapis.com/v1/authorize"
+      end
+
+      it "has the correct token url" do
+        expect(strategy.options.client_options.token_url).to eq "https://webexapis.com/v1/access_token"
       end
     end
   end
